958a18e8a92e5e84c65d3ca82e224188bbd279fc
Smileslime47 <smile_slime_47@outlook.com>
2021-10-17 12:32:09 +0800

    *将BinaryTree修正为interface接口 *将结点ID与结点Data区分开，以便自定显示内容

--- src/TreePrinter.java
+++ src/TreePrinter.java
@@ -1,21 +1,37 @@
 import java.io.*;
 
-class BinaryTree<T>{
-    class Node{
-        Node left=null,right=null;
-        T data;
-
-        Node(T d){data=d;}
-        Node(T d, Node l, Node r){data=d;left=l;right=r;}
-    }
-    Node root;
+/**
+ * 二叉树接口
+ * @param <T> 二叉树存储的数据类型
+ * @param <N> 二叉树的结点类型
+ * e.g. public class Tree implements BinaryTree<Character,Tree.Node>
+ */
+interface BinaryTree<T,N>{
+    N getLeft(N n);
+    N getRight(N n);
+    N getRoot();
+    /**
+     * 返回结点用于显示的内容
+     * @param n 结点
+     * @return 结点存储的的数据类型
+     */
+    T getData(N n);
+    /**
+     * 仅仅用于区分各个结点，对于每个不同的结点返回一个互不相同的数字ID即可
+     * @param n 结点
+     * @return 返回结点ID
+     */
+    Integer getID(N n);
 }
 
 /**
- * 一个Java的二叉树可视化矢量图生成器，基于Graphviz和Emden Gansner的tree.g脚本
+ * 一个Java的二叉树可视化矢量图生成器，基于Graphviz
+ * @param <Tree> 被打印树的类
+ * @param <N> 被打印树的结点类
+ * e.g. new TreePrinter<Tree,Tree.Node>
  * @author smile_slime_47
  */
-public class TreePrinter<Tree extends BinaryTree<?>> {
+public class TreePrinter<Tree extends BinaryTree<?, N>, N> {
 
     //Powershell和Graphviz的环境变量路径，用分号隔开，路径参考：C:\Program Files\Graphviz\bin\;C:\Windows\System32\WindowsPowerShell\v1.0\
     static final String PATH=" ";
@@ -31,16 +47,17 @@ public class TreePrinter<Tree extends BinaryTree<?>> {
     /**
      * 进行先序遍历生成结点的关联代码
      */
-    void printNode(BinaryTree<?>.Node n){
+    void printNode(N n){
         if(n!=null){
             try {
-                if(n.left!=null)out.write("    "+n.data+" -> "+n.left.data+";\n");
-                if(n.right!=null)out.write("    "+n.data+" -> "+n.right.data+";\n");
+                out.write(printObj.getID(n)+" [label=\""+printObj.getData(n)+"\"];\n");
+                if(printObj.getLeft(n)!=null)out.write("    "+printObj.getID(n)+" -> "+printObj.getID(printObj.getLeft(n))+";\n");
+                if(printObj.getRight(n)!=null)out.write("    "+printObj.getID(n)+" -> "+printObj.getID(printObj.getRight(n))+";\n");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            printNode(n.left);
-            printNode(n.right);
+            printNode(printObj.getLeft(n));
+            printNode(printObj.getRight(n));
         }
     }
 
@@ -58,7 +75,7 @@ public class TreePrinter<Tree extends BinaryTree<?>> {
                         node [shape=circle];
                         edge [arrowsize=0.8];
                     """);
-            printNode(printObj.root);
+            printNode(printObj.getRoot());
             out.write("}");
             out.close();
         } catch (IOException e) {
@@ -74,7 +91,7 @@ public class TreePrinter<Tree extends BinaryTree<?>> {
         try {
             out = new BufferedWriter(new FileWriter(fileName+".bat"));
             out.write("SET Path="+PATH+"\n");
-            out.write("powershell.exe dot "+fileName+".dot | gvpr -c -f tree.gv | neato -n -Tsvg -o "+fileName+".svg\n");
+            out.write("powershell.exe dot "+fileName+".dot | neato -n -Tsvg -o "+fileName+".svg\n");
             out.write("EXIT");
             out.close();
         } catch (IOException e) {
@@ -112,11 +129,14 @@ public class TreePrinter<Tree extends BinaryTree<?>> {
             //等待生成svg矢量图
             Thread.sleep(400);
             openImg(fileName);
+
             //删除过程中的中间文件，可以视情况注释掉这段内容
+            Thread.sleep(200);
             File del=new File(fileName+".bat");
             del.delete();
             del=new File(fileName+".dot");
             del.delete();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
